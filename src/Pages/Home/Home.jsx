4848f0e78041ad75e009de822652c8ec923fd98a
ashikur-rahman-10 <ashikur.rahman3912@gmail.com>
2023-08-11 19:31:00 +0600

    some issue fixex

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import TopBanner from "./TopBanner/TopBanner";
 import Solutions from "./Solutions/Solutions";
+import BottomBanner from "./BottomBanner/BottomBanner";
+import PrinctingSection from "./PricingSection/PrinctingSection";
 
 const Home = () => {
     return (
         <div>
             <TopBanner></TopBanner>
             <Solutions></Solutions>
+            <PrinctingSection></PrinctingSection>
+            <BottomBanner></BottomBanner>
         </div>
     );
 };
