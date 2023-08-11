14f3e8818a3efa689a4f0f2ad676af8f08c00f38
Your Name <you@example.com>
2023-08-11 17:33:27 +0600

    solution section added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import TopBanner from "./TopBanner/TopBanner";
+import Solutions from "./Solutions/Solutions";
 
 const Home = () => {
     return (
         <div>
             <TopBanner></TopBanner>
+            <Solutions></Solutions>
         </div>
     );
 };
