f93b9d1bee8b6641f12db734c83eec92821e3d0a
ashikur-rahman-10 <ashikur.rahman3912@gmail.com>
2023-08-18 19:32:37 +0600

    review fetch from backend

--- src/Pages/Home/Review/ReviewAll.jsx
+++ src/Pages/Home/Review/ReviewAll.jsx
@@ -1,27 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import ReviewCart from './ReviewCart';
+import React, { useEffect, useState } from "react";
+import ReviewCart from "./ReviewCart";
 
 const ReviewAll = () => {
     const [reviews, setReview] = useState();
 
     useEffect(() => {
-        fetch("../../../../public/review.json")
-            .then(res => res.json())
-            .then(data => setReview(data))
+        fetch("https://micro-authenticator-server.vercel.app/reviews")
+            .then((res) => res.json())
+            .then((data) => setReview(data));
     }, []);
     // console.log(reviews);
     return (
         <div>
-            <div className='pt-28 lg:mx-40 mb-10 '>
-            <h2 className=" text-center text-4xl text-white font-bold mb-10">
+            <div className="pt-28 lg:mx-40 mb-10 ">
+                <h2 className=" text-center text-4xl text-white font-bold mb-10">
                     All Review
                 </h2>
-                {
-                    reviews?.map(review => <ReviewCart
-                    key={review?.reviewer_name}
-                    review={review}
-                    ></ReviewCart>)
-                }
+                {reviews?.map((review) => (
+                    <ReviewCart key={review?._id} review={review}></ReviewCart>
+                ))}
             </div>
         </div>
     );
--- src/Pages/Home/Review/ReviewCart.jsx
+++ src/Pages/Home/Review/ReviewCart.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const ReviewCart = ({ review }) => {
-    console.log(review);
     const { reviewer_name, review_text, rating } = review;
     return (
-        <div className=''>
-            <div className='p-8 border border-violet-950 rounded-xl my-2 text-white hover:bg-black hover:shadow-2xl hover:shadow-violet-800'>
-                <p className='text-[]'>{review_text}</p>
-                <p className='pt-8 text-[12px] bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-transparent'>Rating: {rating}</p>
+        <div className="max-w-6xl mx-auto">
+            <div className="p-8 border border-violet-950 rounded-xl my-2 text-white hover:bg-black hover:shadow-2xl hover:shadow-violet-800">
+                <p className="text-[]">{review_text}</p>
+                <p className="pt-8 text-[12px] bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-transparent">
+                    Rating: {rating}
+                </p>
 
-                <h3 className=' text-[13px]'>{reviewer_name}</h3>
+                <h3 className=" text-[13px]">{reviewer_name}</h3>
             </div>
         </div>
     );
